b7b8624d3fbe81a3641900e8ba97e42f8a355bfc
Leeiss <113044620+Leeiss@users.noreply.github.com>
2023-05-15 23:58:30 +0300

    сделана регистрация

--- TeamPriject3_СollectionOfAttributes/Registration.Designer.cs
+++ TeamPriject3_СollectionOfAttributes/Registration.Designer.cs
@@ -36,6 +36,12 @@
             this.button_Registr = new System.Windows.Forms.Button();
             this.invented_login = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.closing_panel = new System.Windows.Forms.Panel();
+            this.Entry = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Code = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.closing_panel.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -43,39 +49,41 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
-            this.label1.Location = new System.Drawing.Point(116, 46);
+            this.label1.Location = new System.Drawing.Point(155, 58);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(271, 46);
+            this.label1.Size = new System.Drawing.Size(363, 63);
             this.label1.TabIndex = 0;
             this.label1.Text = "Регистрация";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial Narrow", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
-            this.label2.Location = new System.Drawing.Point(26, 132);
+            this.label2.Location = new System.Drawing.Point(35, 165);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 24);
+            this.label2.Size = new System.Drawing.Size(73, 31);
             this.label2.TabIndex = 1;
             this.label2.Text = "Email";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // invented_email
             // 
-            this.invented_email.Location = new System.Drawing.Point(30, 159);
+            this.invented_email.Location = new System.Drawing.Point(40, 199);
+            this.invented_email.Margin = new System.Windows.Forms.Padding(4);
             this.invented_email.Multiline = true;
             this.invented_email.Name = "invented_email";
-            this.invented_email.Size = new System.Drawing.Size(462, 47);
+            this.invented_email.Size = new System.Drawing.Size(615, 58);
             this.invented_email.TabIndex = 2;
             // 
             // invented_password
             // 
-            this.invented_password.Location = new System.Drawing.Point(30, 382);
+            this.invented_password.Location = new System.Drawing.Point(40, 478);
+            this.invented_password.Margin = new System.Windows.Forms.Padding(4);
             this.invented_password.Multiline = true;
             this.invented_password.Name = "invented_password";
-            this.invented_password.Size = new System.Drawing.Size(462, 47);
+            this.invented_password.Size = new System.Drawing.Size(615, 58);
             this.invented_password.TabIndex = 3;
             // 
             // label3
@@ -83,9 +91,10 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial Narrow", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
-            this.label3.Location = new System.Drawing.Point(26, 355);
+            this.label3.Location = new System.Drawing.Point(35, 444);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(71, 24);
+            this.label3.Size = new System.Drawing.Size(98, 31);
             this.label3.TabIndex = 4;
             this.label3.Text = "Пароль";
             // 
@@ -94,9 +103,10 @@
             this.button_Registr.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(98)))));
             this.button_Registr.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button_Registr.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button_Registr.Location = new System.Drawing.Point(66, 453);
+            this.button_Registr.Location = new System.Drawing.Point(88, 566);
+            this.button_Registr.Margin = new System.Windows.Forms.Padding(4);
             this.button_Registr.Name = "button_Registr";
-            this.button_Registr.Size = new System.Drawing.Size(371, 70);
+            this.button_Registr.Size = new System.Drawing.Size(495, 88);
             this.button_Registr.TabIndex = 5;
             this.button_Registr.Text = "Зарегистрироваться";
             this.button_Registr.UseVisualStyleBackColor = false;
@@ -104,10 +114,11 @@
             // 
             // invented_login
             // 
-            this.invented_login.Location = new System.Drawing.Point(30, 263);
+            this.invented_login.Location = new System.Drawing.Point(40, 329);
+            this.invented_login.Margin = new System.Windows.Forms.Padding(4);
             this.invented_login.Multiline = true;
             this.invented_login.Name = "invented_login";
-            this.invented_login.Size = new System.Drawing.Size(462, 47);
+            this.invented_login.Size = new System.Drawing.Size(615, 58);
             this.invented_login.TabIndex = 7;
             // 
             // label4
@@ -115,18 +126,79 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Arial Narrow", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
-            this.label4.Location = new System.Drawing.Point(26, 236);
+            this.label4.Location = new System.Drawing.Point(35, 295);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(59, 24);
+            this.label4.Size = new System.Drawing.Size(80, 31);
             this.label4.TabIndex = 6;
             this.label4.Text = "Логин";
             // 
+            // closing_panel
+            // 
+            this.closing_panel.Controls.Add(this.Entry);
+            this.closing_panel.Controls.Add(this.label6);
+            this.closing_panel.Controls.Add(this.Code);
+            this.closing_panel.Controls.Add(this.label5);
+            this.closing_panel.Location = new System.Drawing.Point(24, 9);
+            this.closing_panel.Name = "closing_panel";
+            this.closing_panel.Size = new System.Drawing.Size(644, 663);
+            this.closing_panel.TabIndex = 8;
+            this.closing_panel.Visible = false;
+            // 
+            // Entry
+            // 
+            this.Entry.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(98)))));
+            this.Entry.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Entry.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Entry.Location = new System.Drawing.Point(396, 577);
+            this.Entry.Margin = new System.Windows.Forms.Padding(4);
+            this.Entry.Name = "Entry";
+            this.Entry.Size = new System.Drawing.Size(235, 65);
+            this.Entry.TabIndex = 12;
+            this.Entry.Text = "Войти";
+            this.Entry.UseVisualStyleBackColor = false;
+            this.Entry.Click += new System.EventHandler(this.Entry_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
+            this.label6.Location = new System.Drawing.Point(14, 46);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(604, 138);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Подтвердите адрес\r\nэлектронной почты\r\n";
+            // 
+            // Code
+            // 
+            this.Code.Location = new System.Drawing.Point(86, 347);
+            this.Code.Margin = new System.Windows.Forms.Padding(4);
+            this.Code.Multiline = true;
+            this.Code.Name = "Code";
+            this.Code.Size = new System.Drawing.Size(457, 58);
+            this.Code.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
+            this.label5.Location = new System.Drawing.Point(139, 255);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(364, 74);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Введите код, который мы,\r\n         вам отправили";
+            // 
             // Registration
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(235)))), ((int)(((byte)(230)))));
-            this.ClientSize = new System.Drawing.Size(510, 547);
+            this.ClientSize = new System.Drawing.Size(680, 684);
+            this.Controls.Add(this.closing_panel);
             this.Controls.Add(this.invented_login);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button_Registr);
@@ -135,8 +207,11 @@
             this.Controls.Add(this.invented_email);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Registration";
             this.Text = "Registration";
+            this.closing_panel.ResumeLayout(false);
+            this.closing_panel.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -152,5 +227,10 @@
         private System.Windows.Forms.Button button_Registr;
         private System.Windows.Forms.TextBox invented_login;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel closing_panel;
+        private System.Windows.Forms.Button Entry;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Code;
+        private System.Windows.Forms.Label label5;
     }
 }
