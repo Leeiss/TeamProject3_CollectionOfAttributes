a13d0f9c2e6e0c5a3c7f3ef226624fbc84e4d58c
Azatik <azatz739@gmail.com>
2023-05-19 23:30:08 +0300

    Сделанно добавление предметов

--- TeamPriject3_СollectionOfAttributes/Add_new_Furnitures.Designer.cs
+++ TeamPriject3_СollectionOfAttributes/Add_new_Furnitures.Designer.cs
@@ -37,6 +37,11 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.select_poster_btn = new System.Windows.Forms.Button();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // CreateNewAlbum_lable
@@ -63,10 +68,10 @@
             // entered_link
             // 
             this.entered_link.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.entered_link.Location = new System.Drawing.Point(28, 388);
+            this.entered_link.Location = new System.Drawing.Point(21, 243);
             this.entered_link.Multiline = true;
             this.entered_link.Name = "entered_link";
-            this.entered_link.Size = new System.Drawing.Size(503, 47);
+            this.entered_link.Size = new System.Drawing.Size(532, 47);
             this.entered_link.TabIndex = 66;
             // 
             // label2
@@ -75,7 +80,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial Narrow", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
-            this.label2.Location = new System.Drawing.Point(24, 361);
+            this.label2.Location = new System.Drawing.Point(17, 216);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(143, 24);
             this.label2.TabIndex = 65;
@@ -85,10 +90,10 @@
             // textBox1
             // 
             this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.textBox1.Location = new System.Drawing.Point(28, 484);
+            this.textBox1.Location = new System.Drawing.Point(21, 339);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(503, 47);
+            this.textBox1.Size = new System.Drawing.Size(532, 47);
             this.textBox1.TabIndex = 68;
             // 
             // label1
@@ -97,7 +102,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Narrow", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
-            this.label1.Location = new System.Drawing.Point(24, 457);
+            this.label1.Location = new System.Drawing.Point(17, 312);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(130, 24);
             this.label1.TabIndex = 67;
@@ -106,10 +111,10 @@
             // textBox2
             // 
             this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.textBox2.Location = new System.Drawing.Point(28, 571);
+            this.textBox2.Location = new System.Drawing.Point(21, 426);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(503, 47);
+            this.textBox2.Size = new System.Drawing.Size(532, 47);
             this.textBox2.TabIndex = 70;
             // 
             // label3
@@ -118,7 +123,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial Narrow", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
-            this.label3.Location = new System.Drawing.Point(24, 544);
+            this.label3.Location = new System.Drawing.Point(17, 399);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(120, 24);
             this.label3.TabIndex = 69;
@@ -129,19 +134,77 @@
             this.select_poster_btn.BackColor = System.Drawing.SystemColors.Control;
             this.select_poster_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.select_poster_btn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
-            this.select_poster_btn.Location = new System.Drawing.Point(28, 310);
+            this.select_poster_btn.Location = new System.Drawing.Point(21, 525);
             this.select_poster_btn.Margin = new System.Windows.Forms.Padding(2);
             this.select_poster_btn.Name = "select_poster_btn";
-            this.select_poster_btn.Size = new System.Drawing.Size(284, 49);
+            this.select_poster_btn.Size = new System.Drawing.Size(183, 78);
             this.select_poster_btn.TabIndex = 75;
             this.select_poster_btn.Text = "Выбрать изображение";
             this.select_poster_btn.UseVisualStyleBackColor = false;
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.buttonEnter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(98)))));
+            this.buttonEnter.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonEnter.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonEnter.Location = new System.Drawing.Point(319, 525);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(234, 78);
+            this.buttonEnter.TabIndex = 76;
+            this.buttonEnter.Text = "Добавить";
+            this.buttonEnter.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
+            this.label4.Location = new System.Drawing.Point(17, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(203, 24);
+            this.label4.TabIndex = 77;
+            this.label4.Text = "Выберите тип комнаты";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(21, 148);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(209, 28);
+            this.comboBox1.TabIndex = 78;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
+            this.label5.Location = new System.Drawing.Point(288, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(265, 24);
+            this.label5.TabIndex = 79;
+            this.label5.Text = "Выберите категорию предмета";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(292, 147);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(261, 28);
+            this.comboBox2.TabIndex = 80;
             // 
             // Add_new_Furnitures
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(575, 687);
+            this.ClientSize = new System.Drawing.Size(584, 623);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonEnter);
             this.Controls.Add(this.select_poster_btn);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label3);
@@ -169,5 +232,10 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button select_poster_btn;
+        private System.Windows.Forms.Button buttonEnter;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
