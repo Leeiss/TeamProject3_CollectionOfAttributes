f17fcf3559d6285e05cba95a599d48c9c2da095d
Kirill_arsentev <arsen@DESKTOP-4AJNJ1I>
2023-05-19 23:45:13 +0300

    сделан анкор профиля

--- TeamPriject3_СollectionOfAttributes/Profile.Designer.cs
+++ TeamPriject3_СollectionOfAttributes/Profile.Designer.cs
@@ -39,69 +39,69 @@
             // label
             // 
             this.label.AutoSize = true;
-            this.label.Font = new System.Drawing.Font("Arial", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label.Location = new System.Drawing.Point(121, 22);
+            this.label.Location = new System.Drawing.Point(3, 12);
             this.label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label.Name = "label";
-            this.label.Size = new System.Drawing.Size(369, 51);
+            this.label.Size = new System.Drawing.Size(266, 45);
             this.label.TabIndex = 63;
             this.label.Text = "Профиль: name";
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(98)))));
-            this.panel1.Location = new System.Drawing.Point(30, 75);
+            this.panel1.Location = new System.Drawing.Point(2, 59);
             this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(545, 10);
+            this.panel1.Size = new System.Drawing.Size(287, 10);
             this.panel1.TabIndex = 64;
             // 
             // label_login
             // 
             this.label_login.AutoSize = true;
-            this.label_login.Font = new System.Drawing.Font("Arial", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_login.Font = new System.Drawing.Font("Segoe UI Semibold", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label_login.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label_login.Location = new System.Drawing.Point(11, 134);
+            this.label_login.Location = new System.Drawing.Point(11, 89);
             this.label_login.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label_login.Name = "label_login";
-            this.label_login.Size = new System.Drawing.Size(293, 51);
+            this.label_login.Size = new System.Drawing.Size(210, 45);
             this.label_login.TabIndex = 65;
             this.label_login.Text = "Логин: name";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Location = new System.Drawing.Point(11, 217);
+            this.label1.Location = new System.Drawing.Point(13, 144);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(327, 51);
+            this.label1.Size = new System.Drawing.Size(232, 45);
             this.label1.TabIndex = 66;
             this.label1.Text = "Пароль: name";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label2.Location = new System.Drawing.Point(13, 300);
+            this.label2.Location = new System.Drawing.Point(13, 201);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(291, 51);
+            this.label2.Size = new System.Drawing.Size(211, 45);
             this.label2.TabIndex = 67;
             this.label2.Text = "Почта: name";
             // 
             // buttonEnter
             // 
             this.buttonEnter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(98)))));
-            this.buttonEnter.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonEnter.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.buttonEnter.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.buttonEnter.Location = new System.Drawing.Point(76, 404);
+            this.buttonEnter.Location = new System.Drawing.Point(19, 255);
             this.buttonEnter.Margin = new System.Windows.Forms.Padding(2);
             this.buttonEnter.Name = "buttonEnter";
-            this.buttonEnter.Size = new System.Drawing.Size(457, 128);
+            this.buttonEnter.Size = new System.Drawing.Size(228, 56);
             this.buttonEnter.TabIndex = 72;
             this.buttonEnter.Text = "Добавить предмет";
             this.buttonEnter.UseVisualStyleBackColor = false;
@@ -111,13 +111,14 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(650, 553);
+            this.ClientSize = new System.Drawing.Size(291, 322);
             this.Controls.Add(this.buttonEnter);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label_login);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label);
+            this.MinimizeBox = false;
             this.Name = "Profile";
             this.Text = "Profile";
             this.Load += new System.EventHandler(this.Profile_Load);
