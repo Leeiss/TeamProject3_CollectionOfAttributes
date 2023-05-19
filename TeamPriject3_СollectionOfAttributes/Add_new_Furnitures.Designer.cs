207dc1d13a256aa5bd505561d09015c817b31d73
Azatik <azatz739@gmail.com>
2023-05-19 23:21:01 +0300

    ыыыы

--- TeamPriject3_СollectionOfAttributes/Add_new_Furnitures.Designer.cs
+++ TeamPriject3_СollectionOfAttributes/Add_new_Furnitures.Designer.cs
@@ -30,7 +30,13 @@
         {
             this.CreateNewAlbum_lable = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.entered_link = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.select_poster_btn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // CreateNewAlbum_lable
@@ -38,7 +44,7 @@
             this.CreateNewAlbum_lable.AutoSize = true;
             this.CreateNewAlbum_lable.Font = new System.Drawing.Font("Arial", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.CreateNewAlbum_lable.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.CreateNewAlbum_lable.Location = new System.Drawing.Point(129, 9);
+            this.CreateNewAlbum_lable.Location = new System.Drawing.Point(19, 9);
             this.CreateNewAlbum_lable.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.CreateNewAlbum_lable.Name = "CreateNewAlbum_lable";
             this.CreateNewAlbum_lable.Size = new System.Drawing.Size(534, 51);
@@ -51,23 +57,98 @@
             this.panel1.Location = new System.Drawing.Point(8, 71);
             this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(781, 10);
+            this.panel1.Size = new System.Drawing.Size(545, 10);
             this.panel1.TabIndex = 63;
             // 
-            // comboBox1
+            // entered_link
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(260, 188);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(152, 28);
-            this.comboBox1.TabIndex = 65;
+            this.entered_link.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.entered_link.Location = new System.Drawing.Point(28, 388);
+            this.entered_link.Multiline = true;
+            this.entered_link.Name = "entered_link";
+            this.entered_link.Size = new System.Drawing.Size(503, 47);
+            this.entered_link.TabIndex = 66;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
+            this.label2.Location = new System.Drawing.Point(24, 361);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 24);
+            this.label2.TabIndex = 65;
+            this.label2.Text = "Введите ссылку";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox1.Location = new System.Drawing.Point(28, 484);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(503, 47);
+            this.textBox1.TabIndex = 68;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
+            this.label1.Location = new System.Drawing.Point(24, 457);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 24);
+            this.label1.TabIndex = 67;
+            this.label1.Text = "Введите стиль";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox2.Location = new System.Drawing.Point(28, 571);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(503, 47);
+            this.textBox2.TabIndex = 70;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
+            this.label3.Location = new System.Drawing.Point(24, 544);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 24);
+            this.label3.TabIndex = 69;
+            this.label3.Text = "Введите цвет";
+            // 
+            // select_poster_btn
+            // 
+            this.select_poster_btn.BackColor = System.Drawing.SystemColors.Control;
+            this.select_poster_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.select_poster_btn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
+            this.select_poster_btn.Location = new System.Drawing.Point(28, 310);
+            this.select_poster_btn.Margin = new System.Windows.Forms.Padding(2);
+            this.select_poster_btn.Name = "select_poster_btn";
+            this.select_poster_btn.Size = new System.Drawing.Size(284, 49);
+            this.select_poster_btn.TabIndex = 75;
+            this.select_poster_btn.Text = "Выбрать изображение";
+            this.select_poster_btn.UseVisualStyleBackColor = false;
             // 
             // Add_new_Furnitures
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(796, 687);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(575, 687);
+            this.Controls.Add(this.select_poster_btn);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.entered_link);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.CreateNewAlbum_lable);
             this.Controls.Add(this.panel1);
             this.Name = "Add_new_Furnitures";
@@ -81,6 +162,12 @@
 
         private System.Windows.Forms.Label CreateNewAlbum_lable;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox entered_link;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button select_poster_btn;
     }
 }
