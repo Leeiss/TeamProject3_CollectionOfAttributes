c29ed01dd2aafb04134bfa585fcf25ca6ac01bc3
Azatik <azatz739@gmail.com>
2023-05-19 16:46:16 +0300

    s

---
+++ TeamPriject3_СollectionOfAttributes/Album_look.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace TeamPriject3_СollectionOfAttributes
+{
+    partial class Album_look
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Album_look));
+            this.label_go_back = new System.Windows.Forms.Label();
+            this.label_go_forward = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.button_name6 = new System.Windows.Forms.Button();
+            this.button_name5 = new System.Windows.Forms.Button();
+            this.button_name4 = new System.Windows.Forms.Button();
+            this.button_name3 = new System.Windows.Forms.Button();
+            this.button_name2 = new System.Windows.Forms.Button();
+            this.button_name1 = new System.Windows.Forms.Button();
+            this.entered_login = new System.Windows.Forms.TextBox();
+            this.album_call = new System.Windows.Forms.Label();
+            this.CreateNewAlbum_lable = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label_go_back
+            // 
+            this.label_go_back.Image = ((System.Drawing.Image)(resources.GetObject("label_go_back.Image")));
+            this.label_go_back.Location = new System.Drawing.Point(12, 410);
+            this.label_go_back.Name = "label_go_back";
+            this.label_go_back.Size = new System.Drawing.Size(82, 75);
+            this.label_go_back.TabIndex = 74;
+            // 
+            // label_go_forward
+            // 
+            this.label_go_forward.Image = ((System.Drawing.Image)(resources.GetObject("label_go_forward.Image")));
+            this.label_go_forward.Location = new System.Drawing.Point(579, 410);
+            this.label_go_forward.Name = "label_go_forward";
+            this.label_go_forward.Size = new System.Drawing.Size(89, 72);
+            this.label_go_forward.TabIndex = 73;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
+            this.button1.Location = new System.Drawing.Point(404, 714);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 51);
+            this.button1.TabIndex = 72;
+            this.button1.Text = "Отмена";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(98)))));
+            this.buttonEnter.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonEnter.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonEnter.Location = new System.Drawing.Point(539, 714);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(129, 51);
+            this.buttonEnter.TabIndex = 71;
+            this.buttonEnter.Text = "Добавить";
+            this.buttonEnter.UseVisualStyleBackColor = false;
+            // 
+            // button_name6
+            // 
+            this.button_name6.BackColor = System.Drawing.SystemColors.Control;
+            this.button_name6.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_name6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
+            this.button_name6.Location = new System.Drawing.Point(366, 532);
+            this.button_name6.Name = "button_name6";
+            this.button_name6.Size = new System.Drawing.Size(232, 72);
+            this.button_name6.TabIndex = 70;
+            this.button_name6.Text = "Войти";
+            this.button_name6.UseVisualStyleBackColor = false;
+            // 
+            // button_name5
+            // 
+            this.button_name5.BackColor = System.Drawing.SystemColors.Control;
+            this.button_name5.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_name5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
+            this.button_name5.Location = new System.Drawing.Point(366, 410);
+            this.button_name5.Name = "button_name5";
+            this.button_name5.Size = new System.Drawing.Size(232, 75);
+            this.button_name5.TabIndex = 69;
+            this.button_name5.Text = "Войти";
+            this.button_name5.UseVisualStyleBackColor = false;
+            // 
+            // button_name4
+            // 
+            this.button_name4.BackColor = System.Drawing.SystemColors.Control;
+            this.button_name4.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_name4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
+            this.button_name4.Location = new System.Drawing.Point(366, 299);
+            this.button_name4.Name = "button_name4";
+            this.button_name4.Size = new System.Drawing.Size(232, 72);
+            this.button_name4.TabIndex = 68;
+            this.button_name4.Text = "Войти";
+            this.button_name4.UseVisualStyleBackColor = false;
+            // 
+            // button_name3
+            // 
+            this.button_name3.BackColor = System.Drawing.SystemColors.Control;
+            this.button_name3.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_name3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
+            this.button_name3.Location = new System.Drawing.Point(82, 532);
+            this.button_name3.Name = "button_name3";
+            this.button_name3.Size = new System.Drawing.Size(232, 72);
+            this.button_name3.TabIndex = 67;
+            this.button_name3.Text = "Войти";
+            this.button_name3.UseVisualStyleBackColor = false;
+            // 
+            // button_name2
+            // 
+            this.button_name2.BackColor = System.Drawing.SystemColors.Control;
+            this.button_name2.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_name2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
+            this.button_name2.Location = new System.Drawing.Point(82, 410);
+            this.button_name2.Name = "button_name2";
+            this.button_name2.Size = new System.Drawing.Size(232, 72);
+            this.button_name2.TabIndex = 66;
+            this.button_name2.Text = "Войти";
+            this.button_name2.UseVisualStyleBackColor = false;
+            // 
+            // button_name1
+            // 
+            this.button_name1.BackColor = System.Drawing.SystemColors.Control;
+            this.button_name1.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_name1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
+            this.button_name1.Location = new System.Drawing.Point(82, 299);
+            this.button_name1.Name = "button_name1";
+            this.button_name1.Size = new System.Drawing.Size(232, 72);
+            this.button_name1.TabIndex = 65;
+            this.button_name1.Text = "Войти";
+            this.button_name1.UseVisualStyleBackColor = false;
+            // 
+            // entered_login
+            // 
+            this.entered_login.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.entered_login.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
+            this.entered_login.Location = new System.Drawing.Point(21, 177);
+            this.entered_login.Multiline = true;
+            this.entered_login.Name = "entered_login";
+            this.entered_login.Size = new System.Drawing.Size(647, 47);
+            this.entered_login.TabIndex = 64;
+            this.entered_login.Text = "Искать в альбомах";
+            // 
+            // album_call
+            // 
+            this.album_call.AutoSize = true;
+            this.album_call.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.album_call.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(122)))), ((int)(((byte)(121)))));
+            this.album_call.Location = new System.Drawing.Point(21, 113);
+            this.album_call.Name = "album_call";
+            this.album_call.Size = new System.Drawing.Size(376, 37);
+            this.album_call.TabIndex = 63;
+            this.album_call.Text = "Всего альбомов: name";
+            // 
+            // CreateNewAlbum_lable
+            // 
+            this.CreateNewAlbum_lable.AutoSize = true;
+            this.CreateNewAlbum_lable.Font = new System.Drawing.Font("Arial", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CreateNewAlbum_lable.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.CreateNewAlbum_lable.Location = new System.Drawing.Point(83, 21);
+            this.CreateNewAlbum_lable.Name = "CreateNewAlbum_lable";
+            this.CreateNewAlbum_lable.Size = new System.Drawing.Size(499, 51);
+            this.CreateNewAlbum_lable.TabIndex = 62;
+            this.CreateNewAlbum_lable.Text = "Альбомы ваших идей";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(98)))));
+            this.panel1.Location = new System.Drawing.Point(21, 75);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(647, 10);
+            this.panel1.TabIndex = 61;
+            // 
+            // Album_look
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 777);
+            this.Controls.Add(this.label_go_back);
+            this.Controls.Add(this.label_go_forward);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonEnter);
+            this.Controls.Add(this.button_name6);
+            this.Controls.Add(this.button_name5);
+            this.Controls.Add(this.button_name4);
+            this.Controls.Add(this.button_name3);
+            this.Controls.Add(this.button_name2);
+            this.Controls.Add(this.button_name1);
+            this.Controls.Add(this.entered_login);
+            this.Controls.Add(this.album_call);
+            this.Controls.Add(this.CreateNewAlbum_lable);
+            this.Controls.Add(this.panel1);
+            this.Name = "Album_look";
+            this.Text = "Album_look";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_go_back;
+        private System.Windows.Forms.Label label_go_forward;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonEnter;
+        private System.Windows.Forms.Button button_name6;
+        private System.Windows.Forms.Button button_name5;
+        private System.Windows.Forms.Button button_name4;
+        private System.Windows.Forms.Button button_name3;
+        private System.Windows.Forms.Button button_name2;
+        private System.Windows.Forms.Button button_name1;
+        private System.Windows.Forms.TextBox entered_login;
+        private System.Windows.Forms.Label album_call;
+        private System.Windows.Forms.Label CreateNewAlbum_lable;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ TeamPriject3_СollectionOfAttributes/Album_look.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeamPriject3_СollectionOfAttributes
+{
+    public partial class Album_look : Form
+    {
+        public Album_look()
+        {
+            InitializeComponent();
+        }
+    }
+}
